67bf2369a03fce43362974ae74045b4e26cada9d
Amina-Sagheer <sagheeramina32@gmail.com>
2024-06-28 16:36:48 -0600

    Complete main file

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import turtle as t
+from turtle import *
+import pandas
 
+timmy = Turtle()
+my_screen = Screen()
+score = 0
+image = "blank_states_img.gif"
+my_screen.addshape(image)
+t.shape(image)
+while True:
+    my_screen.title(f"{score}/50 States")
+    user_input = textinput(title="U.S. Sates Game", prompt="Enter the name of a US state.")
+    name = user_input.title()
+    names = pandas.read_csv("50_states.csv")
+    if name == "Exit":
+        break
+    for index, row in names.iterrows():
+        if row["state"] == name:
+            x = row["x"]
+            y = row["y"]
+            score += 1
+            timmy.penup()
+            timmy.goto(x, y)
+            timmy.write(name)
+            timmy.hideturtle()
+        else:
+            continue
+
+my_screen.bye()
